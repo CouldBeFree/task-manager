b3526b999f16dde613cdf85b2aff3ca005a01dfc
sasha <propokerstar90@gmail.com>
2019-10-04 18:10:29 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('./db/mongoose');
+const userRouter = require('./routers/user');
+const taskRouter = require('./routers/task');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+});
+
+const bcrypt = require('bcryptjs');
+
+/*const myFunction = async () => {
+    const password = 'Red12345!';
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare('Red12345!', hashedPassword);
+    console.log(isMatch)
+};
+
+myFunction();*/
